944bf10980d7d3a3275161348807784dffef9af4
Megan Wiley <mwiley322@gmail.com>
2017-02-04 17:46:55 -0800

    Add game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,12 @@ var cardThree = "king";
 var cardFour = "king";
 
 console.log(cardThree == cardFour);
+
+
+if (cardOne == cardTwo) {
+  alert("You've found a match!");
+} else if (cardThree == cardFour) {
+  alert("You've found a match!");
+} else {
+  alert("Sorry, try again");
+}
