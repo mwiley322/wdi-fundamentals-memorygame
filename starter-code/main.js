00c55989f369113e57921749a4111fdf3096e0ab
Megan Wiley <mwiley322@gmail.com>
2017-02-05 21:20:51 -0800

    Add createCard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,13 +4,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log(cardThree == cardFour);
+// if (cardOne === cardTwo) {
+//   alert("You've found a match!");
+// } else if (cardThree === cardFour) {
+//   alert("You've found a match!");
+// } else {
+//   alert("Sorry, try again");
+// }
 
 
-if (cardOne == cardTwo) {
-  alert("You've found a match!");
-} else if (cardThree == cardFour) {
-  alert("You've found a match!");
-} else {
-  alert("Sorry, try again");
+var createCards = function() {
+  for (var i = 0; i <= 3; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    document.getElementById('game-board').appendChild(newCard);
+  }
 }
+
+// body.addEventListener('load', createCards);
+createCards();
