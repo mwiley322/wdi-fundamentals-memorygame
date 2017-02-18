b5d7ff5b0191b685b4eec22e4be624f08d8bfccf
Megan Wiley <mwiley322@gmail.com>
2017-02-18 00:11:57 -0800

    add styling and dom logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,8 @@
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
 // if (cardOne === cardTwo) {
 //   alert("You've found a match!");
@@ -13,13 +13,63 @@ var cardFour = "king";
 // }
 
 
-var createCards = function() {
-  for (var i = 0; i <= 3; i++) {
-    var newCard = document.createElement('div');
-    newCard.className = 'card';
-    document.getElementById('game-board').appendChild(newCard);
+// var createCards = function() {
+//   for (var i = 0; i <= 3; i++) {
+//     var newCard = document.createElement('div');
+//     newCard.className = 'card';
+//     document.getElementById('game-board').appendChild(newCard);
+//   }
+// }
+
+// body.addEventListener('load', createCards);
+// createCards();
+
+var cards = ['unicorn', 'tiger', 'unicorn', 'tiger', 'unicorn', 'tiger', 'tiger', 'unicorn', 'tiger', 'tiger', 'unicorn', 'unicorn'];
+var cardsInPlay = [];
+var board = document.getElementById('game-board');
+
+function createCards() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.textContent = '?';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
   }
 }
 
-// body.addEventListener('load', createCards);
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
+     if (this.getAttribute('data-card') === 'tiger') {
+       this.innerHTML = "<img src ='http://i.imgur.com/Sodk5Qe.jpg' alt='Tiger'>";
+    } else {
+      this.innerHTML = "<img src='http://i.imgur.com/DRVkkhB.jpg' alt='Unicorn'>";
+    }
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+// function isMatch(cards) {
+//   if (cards[0] === cards[1]) {
+//     var scoreboard = document.getElementById('scoreboard');
+//     scoreboard.className = 'match';
+//     scoreboard.textContent = "You found a match!";
+//   } else {
+//       alert("Sorry, try again!");
+//   }
+// }
+//
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert('You found a match!');
+  } else {
+      alert("Sorry, try again!");
+  }
+}
+
 createCards();
